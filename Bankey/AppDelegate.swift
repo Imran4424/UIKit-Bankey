610133d4f39a4827835ebbf0089751f75b5bbe67
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-01-01 04:27:59 +0600

    update appdelegate

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -11,13 +11,35 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
+    let loginViewController = LoginViewController()
+    let onboardingContainerViewController = OnboardingContainerViewController()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = LoginViewController()
+        
+        loginViewController.delegate = self
+//        window?.rootViewController = loginViewController
+        
+        onboardingContainerViewController.delegate = self
+        window?.rootViewController = onboardingContainerViewController
         
         return true
+    }
+}
+
+// MARK: - LoginViewControllerDelegate
+extension AppDelegate: LoginViewControllerDelegate {
+    func didLogin() {
+        print("[AppDelegate] Did login")
+    }
+}
+
+// MARK: - OnboardingContainerViewController
+extension AppDelegate: OnboardingContainerViewControllerDelegate {
+    func didFinishOnboarding() {
+        print("[AppDelegate] Did finish Onboarding")
     }
 }
 
