1143bf07992c509a425ad33ba97f2cb1776d6810
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-01-02 00:04:03 +0600

    tabbar

---
+++ Bankey/Utils/UIViewControllerUtils.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewControllerUtils.swift
+//  Bankey
+//
+//  Created by Shah Md Imran Hossain on 1/1/23.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground() // to hide navigation bar
+        navBarAppearance.backgroundColor = appColor
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
